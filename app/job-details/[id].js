c51cd48e0376f8d956bd02df5a3e6df6eda39ee3
sandipthapa99 <sandipthapa386@gmail.com>
2024-01-02 18:01:02 +0545

    feat: Job detail page header section done

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,7 +1,57 @@
-import React from "react";
+import { Stack, useRouter, useGlobalSearchParams } from "expo-router";
+import { useCallback, useState } from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+import {
+  Company,
+  JobAbout,
+  JobFooter,
+  JobTabs,
+  ScreenHeaderBtn,
+  Specifics,
+} from "../../components";
+
+import { COLORS, SIZES, icons } from "../../constants";
+import useFetch from "../../hook/useFetch";
 
 const JobDetails = () => {
-  return <div>JobDetails</div>;
+  const params = useGlobalSearchParams();
+  const router = useRouter();
+
+  const { data, isLoading, error, refetch } = useFetch("job-details", {
+    job_id: params.id,
+  });
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension={"60%"}
+              handlePress={() => router.back()}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.share} dimension={"60%"} />
+          ),
+          headerTitle: "",
+        }}
+      />
+      <>
+        <ScrollView></ScrollView>
+      </>
+    </SafeAreaView>
+  );
 };
 
 export default JobDetails;
