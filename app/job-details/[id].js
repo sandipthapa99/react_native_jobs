010c2dc71ce91a4dd673c013a85f6339befe34ea
sandipthapa99 <sandipthapa386@gmail.com>
2024-01-04 17:41:58 +0545

    feat: Job Company component in detail page

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -24,6 +24,10 @@ const JobDetails = () => {
   const params = useGlobalSearchParams();
   const router = useRouter();
 
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {};
+
   const { data, isLoading, error, refetch } = useFetch("job-details", {
     job_id: params.id,
   });
@@ -48,7 +52,30 @@ const JobDetails = () => {
         }}
       />
       <>
-        <ScrollView></ScrollView>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {isLoading ? (
+            <ActivityIndicator size={"large"} color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+              <JobTabs />
+            </View>
+          )}
+        </ScrollView>
       </>
     </SafeAreaView>
   );
