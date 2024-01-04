e5221a691913bb77d70e944dbcbca462f70873af
sandipthapa99 <sandipthapa386@gmail.com>
2024-01-04 17:57:19 +0545

    feat: job tabs added

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -19,8 +19,10 @@ import {
 
 import { COLORS, SIZES, icons } from "../../constants";
 import useFetch from "../../hook/useFetch";
+const tabs = ["About", "Qualifications", "Responsibilities"];
 
 const JobDetails = () => {
+  const [activeTab, setActiveTab] = useState(tabs[0]);
   const params = useGlobalSearchParams();
   const router = useRouter();
 
@@ -72,7 +74,11 @@ const JobDetails = () => {
                 companyName={data[0].employer_name}
                 location={data[0].job_country}
               />
-              <JobTabs />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
             </View>
           )}
         </ScrollView>
